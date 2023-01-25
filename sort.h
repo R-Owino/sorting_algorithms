267adbc01f2b0c9bdf20842b18c9a97448512a96
Rehema Owino <sayeakinyi@gmail.com>
2023-01-24 18:38:59 -0800

    Header file

--- sort.h
+++ sort.h
@@ -27,20 +27,30 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+
 void quick_sort(int *array, size_t size);
 size_t partition(int *array, ssize_t lo, ssize_t hi, size_t size);
 void quicksort(int *array, ssize_t lo, ssize_t hi, size_t size);
+
 void shell_sort(int *array, size_t size);
+
 void cocktail_sort_list(listint_t **list);
 void swap(listint_t **list, listint_t *node);
+
 void counting_sort(int *array, size_t size);
+
 void merge_sort(int *array, size_t size);
 void merge(size_t start, size_t middle, size_t end, int *dest, int *source);
 void split_merge(size_t start, size_t end, int *array, int *copy);
+
 void heap_sort(int *array, size_t size);
 void sift_down(int *array, size_t root, size_t end, size_t size);
 void make_heap(int *array, size_t size);
+
 void radix_sort(int *array, size_t size);
+unsigned int pow_10(unsigned int power);
+unsigned int count_sort(int *array, size_t size, unsigned int digit);
+
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
