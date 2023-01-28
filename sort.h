a3f9033cb6e15106a008ed1c3ec7df57c71db68c
Rehema Owino <sayeakinyi@gmail.com>
2023-01-28 12:51:34 -0800

    Header file

--- sort.h
+++ sort.h
@@ -55,7 +55,7 @@ void bitonic_sort(int *array, size_t size);
 void bitonic_compare(char up, int *array, size_t size);
 void bitonic_merge(char up, int *array, size_t size);
 void bitonic_merge(char up, int *array, size_t size);
-	
+
 void quick_sort_hoare(int *array, size_t size);
 
 #endif /* SORT_H */
